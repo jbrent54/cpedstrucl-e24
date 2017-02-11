1ac7b086b85830baeaeb6e5bb37768807f7729f3
Brent Magdamit <jbrent56@gmail.com>
2017-02-11 11:50:32 +0800

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+struct Tree{
+	char data;
+	Tree *left, *right, *parent;  
+};
+struct Tree *newTreeNode(int data){
+	Tree *node = new Tree;
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
+	node->parent = NULL;
+	return node;
+}
+struct Tree* insertTreeNode(struct Tree *node, int data){
+	static Tree *p;
+	Tree *retNode;
+	if(node == NULL)  {
+	    retNode = newTreeNode(data);
+	    retNode->parent = p;
+	    return retNode;
+	}
+	if(data <= node->data ){ 
+	    p = node;
+	    node->left = insertTreeNode(node->left,data);
+	} 
+	else{
+	    p = node;
+	    node->right = insertTreeNode(node->right,data);
+	} 
+	return node;
+}
+void printTreeInOrder(struct Tree *node){
+	if(node == NULL) return;
+	printTreeInOrder(node->left);
+	cout << node->data << " ";
+	printTreeInOrder(node->right);
+}
+void printTreePostOrder(struct Tree *node){
+	if(node == NULL) return;
+	printTreePostOrder(node->left);
+	printTreePostOrder(node->right);
+	cout << node->data << " ";
+}
+int main(int argc, char **argv){
+	char ch, ch1, ch2;
+	Tree *found;
+	Tree *succ;
+	Tree *pred;
+	Tree *ancestor;
+	char charArr[9] = {'A','B','C','D','E','F','G','H','I'};
+	Tree *root = newTreeNode('1');
+	insertTreeNode(root,'2');
+	insertTreeNode(root,'3');
+	insertTreeNode(root,'4');
+	insertTreeNode(root,'5');  
+	insertTreeNode(root,'6');
+	insertTreeNode(root,'7');
+	insertTreeNode(root,'8');
+	insertTreeNode(root,'9');
+	cout << "increasing sort order\n";
+	printTreeInOrder(root);
+	cout << endl;
+	cout << "post order \n";
+	printTreePostOrder(root);
+	cout << endl;
+}
